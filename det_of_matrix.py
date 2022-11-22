4e4c6b7fe2d29a08dcc1bf548b1c22fad4d8e1bc
Rarosfp <86689587+rarosfp@users.noreply.github.com>
2022-11-22 18:28:57 +0200

    Update det_of_matrix.py

--- det_of_matrix.py
+++ det_of_matrix.py
@@ -14,23 +14,33 @@ import numpy as np
 
 # ---- Import the matrix in the program ---- #
 #
-# For the dimensions of the first matrix
+# For the dimensions of the Matrix
 #
 
 print('\nImport the number of Rows and Columns for the first Matrix\n',)
 R = int(input("\nEnter the number of rows: "))
 C = int(input("\nEnter the number of columns: "))
+num_in_mat= R * C # calculate the elements in the matrix 
 
-while R != C:
+while R != C :
 
     print('\nThe Matrix Determinant can only be calculated in a square matrix!\n\nImport again the number of Rows and Columns for the Matrix\n',)
     R = int(input("\nEnter the number of rows: "))
     C = int(input("\nEnter the number of columns: "))
-    
+
+
 # For the entries of the first matrix
 print("\nEnter the entries in a single line (separated by space): ")
 matrix = list(map(int, input().split()))
 
+i = 1 # counter for the next while
+
+while num_in_mat != i :
+    # For the entries of the first matrix
+    print("\nThe Matrix with dimensions ",(R, C)," need ", num_in_mat ," of entries! \n\nEnter ",num_in_mat ," entries again in a single line (separated by space): ")
+    matrix = list(map(int, input().split()))
+    i = len(matrix)
+    
 # The matrix
 matrix = np.array(matrix).reshape(R, C)
 
