82b1d5bb9c9929dbf7b9acc059ffdc5e1865c2ab
Rarosfp <86689587+rarosfp@users.noreply.github.com>
2022-11-23 01:26:35 +0200

    Update mat_mult.py

--- mat_mult.py
+++ mat_mult.py
@@ -19,7 +19,7 @@ import numpy as np
 print('\nImport the number of Rows and Columns for the first Matrix\n',)
 R1 = int(input("\nEnter the number of rows: "))
 C1 = int(input("\nEnter the number of columns: "))
-x1 = (R1,C1)
+x1 = (R1, C1)
 num_in_mat1= R1 * C1 # calculate the elements in the first matrix
 
 # For the entries of the first matrix
@@ -27,15 +27,13 @@ num_in_mat1= R1 * C1 # calculate the elements in the first matrix
 print("\nEnter the entries in a single line (separated by space): ")
 
 mat_1 = list(map(int, input().split()))
-
-i = 1 # counter for the next while
+i = len(mat_1)
 
 while num_in_mat1 != i :
     # For the entries of the first matrix
     print("\nThe Matrix with dimensions ",(R1, C1)," need ", num_in_mat1 ," of entries! \n\nEnter ",num_in_mat1 ," entries again in a single line (separated by space): ")
     mat_1 = list(map(int, input().split()))
     i = len(mat_1)
-    
 # For printing the first matrix
 #
 mat_1 = np.array(mat_1).reshape(R1, C1)
@@ -54,7 +52,7 @@ C2 = int(input("\nEnter the number of columns: "))
 x2=(R2, C2)
 num_in_mat2= R2 * C2 # calculate the elements in the second matrix
 
-# For the entries of the Second matrix
+#For the entries of the Second matrix
 #
 #The if statement investigates the dimensions of the second matrix.
 #If the dimension is equal to the dimension of the first matrix,
@@ -62,8 +60,15 @@ num_in_mat2= R2 * C2 # calculate the elements in the second matrix
 #Otherwise, continue.
 
 if x1 == x2:
+    
     print("\nEnter the entries in a single line (separated by space): ")
     mat_2 = list(map(int, input().split()))
+    i = len(mat_2)
+    
+    while num_in_mat2 != i :
+        print("\nThe Matrix with dimensions ",(R2, C2)," need ", num_in_mat2 ," of entries! \n\nEnter ",num_in_mat2 ," entries again in a single line (separated by space): ")
+        mat_2 = list(map(int, input().split()))
+        i = len(mat_2)
     # For printing the second matrix
     #
     mat_2 = np.array(mat_2).reshape(C2, R2)
@@ -72,10 +77,16 @@ if x1 == x2:
     #
     dim_2=mat_2.shape
     print('\nThe Second matrix dimensios are: \n', dim_2)
-
+    
 elif C1 == R2:
     print("\nEnter the entries in a single line (separated by space): ")
     mat_2 = list(map(int, input().split()))
+    i = len(mat_2)
+    
+    while num_in_mat2 != i :
+        print("\nThe Matrix with dimensions ",(R2, C2)," need ", num_in_mat2 ," of entries! \n\nEnter ",num_in_mat2 ," entries again in a single line (separated by space): ")
+        mat_2 = list(map(int, input().split()))
+        i = len(mat_2)
     # For printing the second matrix
     #
     mat_2 = np.array(mat_2).reshape(R2, C2)
@@ -84,18 +95,42 @@ elif C1 == R2:
     #
     dim_2=mat_2.shape
     print('\nThe Second matrix dimensios are: \n', dim_2)
+
+elif C1 == C2:
+    print("\nEnter the entries in a single line (separated by space): ")
+    mat_2 = list(map(int, input().split()))
+    i = len(mat_2)
+    
+    while num_in_mat2 != i :
+        print("\nThe Matrix with dimensions ",(R2, C2)," need ", num_in_mat2 ," of entries! \n\nEnter ",num_in_mat2 ," entries again in a single line (separated by space): ")
+        mat_2 = list(map(int, input().split()))
+        i = len(mat_2)
+    # For printing the second matrix
+    #
+    mat_2 = np.array(mat_2).reshape(C2, R2)
+    print('\nThe Second matrix is: \n', mat_2)
+    # For printing the first matrix dimensions
+    #
+    dim_2=mat_2.shape
+    print('\nThe Second matrix dimensios are: \n', dim_2)
     
 else:
-    print('Calculation error!\n In matrix multiplication, the columns of Matrix A must equal the rows of Matrix B.')
-        
-i = 1 # counter for the next while
-
-while num_in_mat2 != i :
-
-    print("\nThe Matrix with dimensions ",(R, C)," need ", num_in_mat ," of entries! \n\nEnter ",num_in_mat ," entries again in a single line (separated by space): ")
+    print('\nCalculation error!\n\nIn matrix multiplication, the columns of Matrix A must equal the rows of Matrix B. (x*z)(z*y)=(x*y)')
+    print('\n\nImport again the number of Rows and Columns for the new Matrix\n')
+    R2 = int(input("\nEnter the number of rows: "))
+    C2 = int(input("\nEnter the number of columns: "))
+    x2=(R2, C2)
+    num_in_mat2= R2 * C2
+    
     if x1 == x2:
         print("\nEnter the entries in a single line (separated by space): ")
         mat_2 = list(map(int, input().split()))
+        i = len(mat_2)
+    
+        while num_in_mat2 != i :
+            print("\nThe Matrix with dimensions ",(R2, C2)," need ", num_in_mat2 ," of entries! \n\nEnter ",num_in_mat2 ," entries again in a single line (separated by space): ")
+            mat_2 = list(map(int, input().split()))
+            i = len(mat_2)
         # For printing the second matrix
         #
         mat_2 = np.array(mat_2).reshape(C2, R2)
@@ -104,10 +139,16 @@ while num_in_mat2 != i :
         #
         dim_2=mat_2.shape
         print('\nThe Second matrix dimensios are: \n', dim_2)
-
+    
     elif C1 == R2:
         print("\nEnter the entries in a single line (separated by space): ")
         mat_2 = list(map(int, input().split()))
+        i = len(mat_2)
+    
+        while num_in_mat2 != i :
+            print("\nThe Matrix with dimensions ",(R2, C2)," need ", num_in_mat2 ," of entries! \n\nEnter ",num_in_mat2 ," entries again in a single line (separated by space): ")
+            mat_2 = list(map(int, input().split()))
+            i = len(mat_2)
         # For printing the second matrix
         #
         mat_2 = np.array(mat_2).reshape(R2, C2)
@@ -116,9 +157,23 @@ while num_in_mat2 != i :
         #
         dim_2=mat_2.shape
         print('\nThe Second matrix dimensios are: \n', dim_2)
-
-    else:
-        print('Calculation error!\n In matrix multiplication, the columns of Matrix A must equal the rows of Matrix B.')
+    elif C1 == C2:
+        print("\nEnter the entries in a single line (separated by space): ")
+        mat_2 = list(map(int, input().split()))
+        i = len(mat_2)
+    
+        while num_in_mat2 != i :
+            print("\nThe Matrix with dimensions ",(R2, C2)," need ", num_in_mat2 ," of entries! \n\nEnter ",num_in_mat2 ," entries again in a single line (separated by space): ")
+            mat_2 = list(map(int, input().split()))
+            i = len(mat_2)
+        # For printing the second matrix
+        #
+        mat_2 = np.array(mat_2).reshape(C2, R2)
+        print('\nThe Second matrix is: \n', mat_2)
+        # For printing the first matrix dimensions
+        #
+        dim_2=mat_2.shape
+        print('\nThe Second matrix dimensios are: \n', dim_2)
 
 # ---- Multiply the matricies ---- #
 
