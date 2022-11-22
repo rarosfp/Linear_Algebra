eb9c298a9a68aadff261dc9c7bb989076e6aef2b
Rarosfp <86689587+rarosfp@users.noreply.github.com>
2022-11-22 18:11:53 +0200

    Update det_of_matrix.py

--- det_of_matrix.py
+++ det_of_matrix.py
@@ -1,5 +1,5 @@
 ##
-##\-\-\-\-\-\-\-\-\-\-\-\ Determinant of matrix calculator -\-\-\-\-\-\-\-\-#
+##\-\-\-\-\-\-\-\-\-\-\ Determinant of matrix calculator \-\-\-\-\-\-\-\-\-\-#
 ##
 ##
 ##\-\-\-\-\-\-\-\-\-\-\-\-\-\-\ By rarosfp \-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-\-#
@@ -16,29 +16,33 @@ import numpy as np
 #
 # For the dimensions of the first matrix
 #
+
 print('\nImport the number of Rows and Columns for the first Matrix\n',)
 R = int(input("\nEnter the number of rows: "))
 C = int(input("\nEnter the number of columns: "))
 
+while R != C:
+
+    print('\nThe Matrix Determinant can only be calculated in a square matrix!\n\nImport again the number of Rows and Columns for the Matrix\n',)
+    R = int(input("\nEnter the number of rows: "))
+    C = int(input("\nEnter the number of columns: "))
+    
 # For the entries of the first matrix
 print("\nEnter the entries in a single line (separated by space): ")
-
 matrix = list(map(int, input().split()))
-  
-# For the matrix
+
+# The matrix
 matrix = np.array(matrix).reshape(R, C)
 
-# Matrix dimensions
+# The dimensions of the matrix
 dim=matrix.shape
 
 # Displaying the Matrix
-print("Numpy Matrix is:\n")
+print("\nThe Matrix is:\n")
 print(matrix)
 
-
-# calculating the determinant of matrix
+# calculating and displaying the determinant of matrix
 det = np.linalg.det(matrix)
-  
 print("\nDeterminant of given ",dim ," matrix is: \n")
 print(int(det))
 
